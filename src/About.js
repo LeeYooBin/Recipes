2e09e2607b42a4dc9e23abfb30c015be54ca5365
TeacherSamanthaSilva <samanthaadelinecordova@outlook.com>
2022-06-17 19:23:54 -0300

    finalizando about
    nesse commit finalizei o about

---
+++ src/About.js
@@ -0,0 +1,22 @@
+import React,{Component} from 'react';
+
+class About extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+
+
+    render(){
+        return(
+            <div className="About">
+                <img class="About" src="../pubic/images/colher.png" alt="" width="24" height="24"/>
+                <h2>About</h2>
+                <p>Mussum Ipsum, cacilds vidis litro abertis. Leite de capivaris, leite de mula manquis sem cabeça.Interagi no mé, cursus quis, vehicula ac nisi.Mais vale um bebadis conhecidiss, que um alcoolatra anonimis.Si num tem leite então bota uma pinga aí cumpadi!</p>
+            </div>
+            );
+        }
+    }
+    
+    export default Footer;
