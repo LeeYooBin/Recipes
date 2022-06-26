208612cf8dfe248ead378e284778087a3aacd766
LeeYooBin <santosroberdan25@gmail.com>
2022-06-25 23:29:42 -0300

    atualizando about

---
+++ src/components/About/Index.jsx
@@ -0,0 +1,24 @@
+import * as S from './style';
+import aboutImg from '../../img/colher.png';
+
+export default function About(){
+    return(
+        <S.About>
+            <S.Image src={aboutImg} alt="" />
+            <S.Wrapper>
+                <S.Title>ABOUT</S.Title>
+                <S.Line className='line'></S.Line>
+                <S.Text>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tincidunt, 
+                    tortor nec rhoncus dictum, lorem massa tempus sem, eu tincidunt libero 
+                    velit sit amet velit. Nunc in euismod urna. Duis dapibus, elit eu eleifend 
+                    tincidunt, nulla ipsum consectetur lorem, quis tempor lorem justo quis nisi. 
+                    Nam interdum, nisi nec mollis sagittis, enim risus euismod nisi, quis rutrum 
+                    quam augue id mauris. Pellentesque mattis hendrerit semper. Orci varius natoque 
+                    penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut vestibulum 
+                    nisl ante, et ultricies sapien facilisis aliquam.
+                </S.Text>
+            </S.Wrapper>
+        </S.About>
+    );
+}
--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,12 +1,14 @@
 import './style.css';
 import Header from "../../components/Header";
 import Recipes from '../../components/Recipes';
+import About from '../../components/About/Index';
 
 export default function Main(){
     return(
         <>
             <Header />
             <Recipes />
+            <About />
         </>
     );
 }
